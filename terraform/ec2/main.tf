4a203167db4203e62100587eadca0e9083efb969
Amal Raj <amalraj.a@palebluesoftware.com>
2025-09-02 14:13:41 +0530

    issue fix

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -1,48 +1,15 @@
-resource "aws_iam_role" "jenkins_role" {
-  name = "jenkins-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-  })
+resource "aws_key_pair" "jenkins-server_key" {
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
 }
 
-resource "aws_iam_policy" "eks_policy" {
-  name        = "eks-describe-list-policy"
-  description = "Policy to allow DescribeCluster and ListClusters on EKS"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = [
-          "eks:DescribeCluster",
-          "eks:ListClusters"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "jenkins_attach" {
-  role       = aws_iam_role.jenkins_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-}
-
-# --- Attach Policy to Role ---
-resource "aws_iam_role_policy_attachment" "eks_attach" {
-  role       = aws_iam_role.jenkins_role.name
-  policy_arn = aws_iam_policy.eks_policy.arn
-}
-
-resource "aws_iam_instance_profile" "jenkins_profile" {
-  name = "jenkins-profile"
-  role = aws_iam_role.jenkins_role.name
+resource "aws_instance" "jenkins" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.security_group_id]
+  iam_instance_profile        = var.iam_instance_profile
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.jenkins-server_key.key_name
+  user_data                   = file(var.user_data_path)
 }
--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -13,12 +13,36 @@ resource "aws_iam_role" "jenkins_role" {
   })
 }
 
-resource "aws_iam_instance_profile" "jenkins_profile" {
-  name = "jenkins-profile"
-  role = aws_iam_role.jenkins_role.name
+resource "aws_iam_policy" "eks_policy" {
+  name        = "eks-describe-list-policy"
+  description = "Policy to allow DescribeCluster and ListClusters on EKS"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "eks:DescribeCluster",
+          "eks:ListClusters"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "jenkins_attach" {
   role       = aws_iam_role.jenkins_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+# --- Attach Policy to Role ---
+resource "aws_iam_role_policy_attachment" "eks_attach" {
+  role       = aws_iam_role.jenkins_role.name
+  policy_arn = aws_iam_policy.eks_policy.arn
+}
+
+resource "aws_iam_instance_profile" "jenkins_profile" {
+  name = "jenkins-profile"
+  role = aws_iam_role.jenkins_role.name
 }
